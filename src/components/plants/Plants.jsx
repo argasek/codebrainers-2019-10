71b0a45ced5f1bc844398053da161307a67a451c
akroterion <akroterion@outlook.com>
2020-04-18 12:23:41 +0200

    Remove form code from Plants.jsx

--- src/components/plants/Plants.jsx
+++ src/components/plants/Plants.jsx
@@ -1,8 +1,6 @@
-import { Button, Card, CardBody, Col, FormGroup, Input, Label, Row } from "reactstrap";
-import Select from "components/shared/Select";
-import { someArray, someOtherArray } from "constants/PlantConstants";
+import { Card, CardBody } from "reactstrap";
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import axios from "axios";
 import Plant from "components/plants/Plant";
 import InProgress from "components/shared/InProgress";
@@ -10,7 +8,6 @@ import InProgress from "components/shared/InProgress";
 const PLANTS_FETCH_DELAY = 250;
 
 class Plants extends React.PureComponent {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -21,18 +18,17 @@ class Plants extends React.PureComponent {
   }
 
   componentDidMount() {
-    this.fetchPlants()
-      .finally(() => {
-        this.setState({ inProgress: false });
-      })
-
+    this.fetchPlants().finally(() => {
+      this.setState({ inProgress: false });
+    });
   }
 
   fetchPlants() {
-    const requestUrl = 'http://gentle-tor-07382.herokuapp.com/plants/';
+    const requestUrl = "http://gentle-tor-07382.herokuapp.com/plants/";
     this.setState({ inProgress: true });
     return this.props.delayFetch(PLANTS_FETCH_DELAY, (resolve, reject) => {
-      axios.get(requestUrl)
+      axios
+        .get(requestUrl)
         .then((response) => {
           const data = response.data;
           const plants = data.map((item) => item.name);
@@ -48,78 +44,28 @@ class Plants extends React.PureComponent {
   }
 
   render() {
-    const {
-      fertilizingFrequency,
-      inputOnChange,
-      plantName,
-      someSelectField,
-    } = this.props;
-
-    const {
-      plants,
-      successPlants,
-      inProgress,
-    } = this.state;
+    const { plants, successPlants, inProgress } = this.state;
 
     return (
       <Card className="mb-4">
         <CardBody>
-          <form method="GET">
-            <Row>
-              <Col xs={6} lg={3}>
-                <Select name="someSelectField" value={someSelectField} onChange={inputOnChange} options={someArray} label="Jakieś pole" />
-              </Col>
-              <Col xs={6} lg={5}>
-                <Select name="fertilizingFrequency" value={fertilizingFrequency} onChange={inputOnChange} options={someOtherArray} label="Fertilizing frequency" />
-              </Col>
-              <Col xs={12} lg={4}>
-                <FormGroup>
-                  <Label for="plantName">Plant name:</Label>
-                  <Input
-                    id="plantName"
-                    name="plantName"
-                    type="text"
-                    value={plantName}
-                    onChange={inputOnChange}
-                  />
-                </FormGroup>
-              </Col>
-            </Row>
-
-            <Button type="submit" className="mt-3">Wyślij formularz</Button>
-          </form>
-          <hr />
           <InProgress inProgress={inProgress} />
-          {
-            successPlants === false &&
-            <p>Nie udało się pobrać Kwiatow</p>
-          }
-          {
-            successPlants &&
+          {successPlants === false && <p>Nie udało się pobrać Kwiatow</p>}
+          {successPlants && (
             <div className="plants">
-              {
-                plants.map((plant, index, arr) =>
-                  <Plant
-                    name={plant}
-                    key={index}
-                  />
-                )
-              }
+              {plants.map((plant, index, arr) => (
+                <Plant name={plant} key={index} />
+              ))}
             </div>
-          }
+          )}
         </CardBody>
       </Card>
-    )
+    );
   }
-
 }
 
 Plants.propTypes = {
-  fertilizingFrequency: PropTypes.string.isRequired,
-  inputOnChange: PropTypes.func.isRequired,
-  plantName: PropTypes.string.isRequired,
-  someSelectField: PropTypes.string.isRequired,
-}
-
+  delayFetch: PropTypes.func.isRequired,
+};
 
 export default Plants;
